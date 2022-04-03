8bdc0de681d90941b11d7984b8f49e753cfab195
Marcin Wojczal <wojczal.m@gmail.com>
2022-04-03 17:44:02 +0200

    Email address validation added. Register button added

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -3,16 +3,18 @@ import {Dialog,
     DialogTitle,
     TextField,
     Button,
-    DialogContent
+    DialogContent,
+    DialogActions
 } from "@mui/material";
 
 // * others
 import PropTypes from 'prop-types';
-
-import React from "react";
+import isEmail from 'validator/lib/isEmail';
+import React, {useState} from "react";
 
 // * icons
 import LoginIcon from '@mui/icons-material/Login';
+import PersonIcon from '@mui/icons-material/Person';
 
 export default function SignIn(props) {
 
@@ -20,47 +22,93 @@ export default function SignIn(props) {
         props.setOpenSignIn(false);
     };
 
+    const [email, setEmail] = useState('');
+    const [isEmailValid, setIsEmailValid] = useState(false);
+
+    const handleEmailChange = event => {
+        const emailValue = event.target.value;                
+
+        if(isEmail(emailValue)) {
+            setIsEmailValid(true);           
+        } else {
+            setIsEmailValid(false);        
+        }
+        setEmail(emailValue);                
+    }
+
     return (
-        <Dialog onClose={handleSignInClose} open={props.openSignIn}>
-            <DialogContent
-                sx={{
-                }}
-            >
-                <DialogTitle>Sign in</DialogTitle>
+        <Dialog 
+            onClose={handleSignInClose} 
+            open={props.openSignIn} 
+            fullWidth
+            maxWidth="xs"
+        >
+             {/* Title */}
+            <DialogTitle>Sign in</DialogTitle>
+            <DialogContent dividers>
+                {/* Email text field */}
                 <TextField 
                      sx={{
                         display: 'block',
-                        width: 'fit-content',
                         margin: '10% 0'
                       }}
                     id="filled-basic" 
-                    label="Login" 
+                    label="E-mail" 
                     variant="filled"
+                    fullWidth
+                    onChange={event => handleEmailChange(event)}
+                    error={!isEmailValid && email != ''}
+                    helperText={!isEmailValid && email != '' ? `Wrong e-mail address!` : ""}
                 />
+                 {/* Password text field */}
                 <TextField 
                      sx={{
                         display: 'block',
-                        width: 'fit-content',
                         margin: '10% 0'
                       }}
                     id="filled-basic" 
                     label="Password" 
                     variant="filled" 
+                    fullWidth
                 />
+                <DialogActions>
+                {/* Sign in button */}  
+                <Button 
+                        sx={{
+                            display: 'block', 
+                            marginLeft: 'auto', 
+                            marginRight: '0',
+                            bgcolor: 'primary.turquoise',
+                            color: 'primary.main',
+                            "&:hover": {
+                                backgroundColor: "primary.turquoiseDark",
+                            }
+                            }} 
+                            variant="contained">
+                        Sign in
+                        <LoginIcon sx={{float: 'right'}}/>
+                    </Button>
+                </DialogActions>
+            </DialogContent>
+            <DialogContent>
+                {/* Register button */}
                 <Button 
                     sx={{
                         display: 'block', 
-                        width: '60%', 
-                        marginLeft: 'auto', 
-                        marginRight: '0',
+                        width: '90%',
+                        margin: '5%',
                         bgcolor: 'primary.turquoise',
-                        color: 'primary.main'
+                        color: 'primary.main',
+                        "&:hover": {
+                            backgroundColor: "primary.turquoiseDark",
+                        }
                         }} 
                         variant="contained">
-                    Sign in
-                    <LoginIcon sx={{float: 'right'}}/>
+                    Create account
+                    <PersonIcon sx={{float: 'right'}}/>
                 </Button>
             </DialogContent>
+            
         </Dialog>
     )
 }
--- src/themes/mainTheme.js
+++ src/themes/mainTheme.js
@@ -6,7 +6,8 @@ const mainTheme = createTheme({
   palette: {
     primary: {
       main: '#ffffff',      // white
-      turquoise: '#0097b8'
+      turquoise: '#0097b8',
+      turquoiseDark: '#00768f'
     },
     secondary: {
       main: '#000000'       // black
